6620f8072bdfa22d8f9b59f452b00505cc60c56d
James Morris <blondfro@gmail.com>
2020-04-05 21:57:14 -0500

    added in new state to store all the to do items created.

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -5,12 +5,27 @@ class ToDo extends React.Component {
     super(props);
     this.state = {
       input: "",
+      list: [],
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
     this.setState({ input: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    /*
+    create a tmp var to store the current value of the list in state.
+     push the current value of the input field to the tmp list.
+     set state of the list to be the value of the tmp list.
+    */
+
+    let tmpList = [...this.state.list]; // using the spread operator
+    tmpList.push(this.state.input); // pushing in the current value of the input field.
+    this.setState({ list: [...tmpList] }); // use the spread operator to push in the updated list.
   }
 
   render() {
@@ -21,7 +36,7 @@ class ToDo extends React.Component {
           value={this.state.input}
           onChange={this.handleChange}
         />
-        <button>Add</button>
+        <button onClick={this.handleSubmit}>Add</button>
       </div>
     );
   }
